1f7b6e6c04d9424175d879470cddb3f986feae6e
adino wayne <gnurt10@gmail.com>
2019-03-20 23:00:17 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CourseComponent } from './course.component';
+import { CoursesService } from './courses.service';
+import { SummaryPipe } from './summary.pipe';
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryPipe,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    CoursesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
